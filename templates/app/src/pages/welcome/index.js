594ef724c76053bc7f4c581931c934022dbe9f6c
JerryC <huangpeijie@youzan.com>
2019-05-09 21:47:24 +0800

    feat: update welcome page

--- templates/app/src/pages/welcome/index.js
+++ templates/app/src/pages/welcome/index.js
@@ -1,7 +1,14 @@
 import MyPage from '../../libs/my-page';
+import { getGlobalView, sleep } from '../../libs/utils/helper';
 
 MyPage({
     onLoad() {
-        console.log('Welcome to BeautyWe.js!!!');
+        // the demo just for show
+        const globalView = getGlobalView(this);
+        return Promise
+            .resolve()
+            .then(() => globalView.showLoading())
+            .then(() => sleep(2000))
+            .then(() => globalView.hideLoading());
     },
 });
