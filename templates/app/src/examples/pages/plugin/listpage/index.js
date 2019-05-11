f8222bfc1892e4eae6a506532122507d4430a376
JerryC <huangjerryc@gmail.com>
2019-05-11 22:32:18 +0800

    feat: add listpage example and import animate.css

--- templates/app/src/examples/pages/plugin/listpage/index.js
+++ templates/app/src/examples/pages/plugin/listpage/index.js
@@ -1,32 +1,36 @@
 import MyPage from '../../../../libs/my-page';
 import { plugin, beautywe } from '../../../../npm/index';
+import { getGlobalView, sleep } from '../../../../libs/utils/helper';
 
-const myPage = new beautywe.BtPage({
-    data: {
-        name: 'listpage',
-    },
-    onLoad() {
-        // 手动加载页面数据
-        const picturesList = this.listPage.getList('pictures');
-
-        // 获取下一页数据，并同步数据到 data
-        picturesList.nextPage().then(() => picturesList.updateData());
-    },
-});
+const myPage = new beautywe.BtPage({});
 
 myPage.use(plugin.listpage({
     lists: [{
-        name: 'pictures',  // 数据名
-        pageSize: 5,   // 每页多少条数据，默认 10
+
+         // 数据名
+        name: 'pictures',
+
+        // 每页多少条数据，默认 10
+        pageSize: 5,
 
         // 每一页的数据源，没次加载页面时，会调用函数，然后取返回的数据。
-        fetchPageData({ pageNo, pageSize }) {
-            const pictures = Array(pageSize).fill(null).map((value, index) => `https://picsum.photos/id/${pageSize * pageNo + index}/400/300`);
-            return Promise.resolve(pictures);
+        fetchPageData({ pageNo, pageSize, theHost }) {
+            const globalView = getGlobalView(theHost);
+            const pictures = Array(pageSize).fill(null).map((value, index) => `https://picsum.photos/id/${pageSize * pageNo + index + 10}/400/300`);
+            return Promise
+                .resolve(pictures)
+                .then(() => globalView && globalView.showLoading())
+                .then(() => sleep(1000))
+                .then(() => globalView && globalView.hideLoading())
+                .then(() => pictures);
         },
     }],
-    onPullDownRefresh: true,    // 开启下拉重载， 默认 false
-    onReachBottom: true,    // 开启上拉加载， 默认 false
+
+    // 开启下拉重载， 默认 false
+    enabledPullDownRefresh: true,
+
+    // 开启上拉加载， 默认 false
+    enabledReachBottom: true,
 }));
 
 MyPage(myPage);
--- templates/app/src/pages/welcome/index.js
+++ templates/app/src/pages/welcome/index.js
@@ -1,14 +1,8 @@
 import MyPage from '../../libs/my-page';
-import { getGlobalView, sleep } from '../../libs/utils/helper';
 
 MyPage({
     onLoad() {
         // the demo just for show
-        const globalView = getGlobalView(this);
-        return Promise
-            .resolve()
-            .then(() => globalView.showLoading())
-            .then(() => sleep(2000))
-            .then(() => globalView.hideLoading());
+        console.log('Wellcome to BeautyWe.js !');
     },
 });
