8396d8dd8312e894c7546730e4f63364c21c81bd
JerryC <huangjerryc@gmail.com>
2019-05-13 23:27:30 +0800

    feat: add logger plugin

---
+++ templates/app/src/libs/plugins/logger.js
@@ -0,0 +1,6 @@
+import { plugin } from '../../npm/index';
+import config from '../../config/index';
+
+const logger = plugin.logger(config.logger);
+
+export default logger;
--- templates/app/src/npm/index.js
+++ templates/app/src/npm/index.js
@@ -1,11 +1,13 @@
 import storage from '@beautywe/plugin-storage';
 import event from '@beautywe/plugin-event';
 import listpage from '@beautywe/plugin-listpage';
+import logger from '@beautywe/plugin-logger';
 
 export const plugin = {
     storage,
     event,
     listpage,
+    logger,
 };
 
 export { default as beautywe } from '@beautywe/core';
