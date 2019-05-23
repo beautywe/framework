12d4332daa9f445f1187bf22d0b9a3fdc4ef340e
JerryC <huangpeijie@youzan.com>
2019-05-23 15:07:56 +0800

    fix: fix sass-min errors

--- templates/app/gulpfile.js/sass-min.js
+++ templates/app/gulpfile.js/sass-min.js
@@ -1,14 +1,14 @@
-const gulp = require('gulp');
 const csso = require('gulp-csso');
 const gulpConfig = require('./config');
 const sass = require('./sass');
 
 function sassMin() {
-    const { from, to } = gulpConfig.sass;
-    return sass
-        .doCompile(gulp.src(from))
-        .pipe(csso())
-        .pipe(gulp.dest(to));
+    return sass.doCompile({
+        ...gulpConfig.sass,
+        afterCompiled: [
+            stream => stream.pipe(csso()),
+        ],
+    });
 }
 
 module.exports = {
--- templates/app/gulpfile.js/sass.js
+++ templates/app/gulpfile.js/sass.js
@@ -7,7 +7,7 @@ const path = require('path');
 const clean = require('gulp-clean');
 const gulpConfig = require('./config');
 
-function doCompile({ from, to }) {
+function doCompile({ from, to, afterCompiled = [] }) {
     const { whiteListForImport } = gulpConfig.sass;
     const toRemoveFiles = new Set();
 
@@ -16,42 +16,49 @@ function doCompile({ from, to }) {
 
         // 编译 sass
         .then(() => new Promise((resolve, reject) => {
-            gulp
-                .src(from)
+            // 编译任务
+            afterCompiled.unshift(
+                gulp
+                    .src(from)
 
-                // 过滤 import 白名单，并记录到待删除列表
-                .pipe(tap((file) => {
-                    const filePath = path.dirname(file.path);
-                    const content = file.contents.toString();
-                    content.replace(/@import\s+['|"](.+)['|"];/g, ($1, $2) => {
-                        const isWhite = (whiteListForImport.filter(item => $2.indexOf(item) > -1)).length > 0;
-                        if (isWhite) {
-                            const rmPath = path.join(filePath, $2);
-                            const filea = rmPath.replace(/src/, 'dist').replace(/\.scss/, '.wxss');
-                            toRemoveFiles.add(filea);
-                        }
-                    });
-                }))
+                    // 过滤 import 白名单，并记录到待删除列表
+                    .pipe(tap((file) => {
+                        const filePath = path.dirname(file.path);
+                        const content = file.contents.toString();
+                        content.replace(/@import\s+['|"](.+)['|"];/g, ($1, $2) => {
+                            const isWhite = (whiteListForImport.filter(item => $2.indexOf(item) > -1)).length > 0;
+                            if (isWhite) {
+                                const rmPath = path.join(filePath, $2);
+                                const filea = rmPath.replace(/src/, 'dist').replace(/\.scss/, '.wxss');
+                                toRemoveFiles.add(filea);
+                            }
+                        });
+                    }))
 
-                // 对 import 进行注释，绕过 sass 编译
-                .pipe(replace(/(@import.+;)/g, ($1, $2) => {
-                    const isWhite = (whiteListForImport.filter(item => $1.indexOf(item) > -1)).length > 0;
-                    return isWhite ? $2 : `/** ${$2} **/`;
-                }))
+                    // 对 import 进行注释，绕过 sass 编译
+                    .pipe(replace(/(@import.+;)/g, ($1, $2) => {
+                        const isWhite = (whiteListForImport.filter(item => $1.indexOf(item) > -1)).length > 0;
+                        return isWhite ? $2 : `/** ${$2} **/`;
+                    }))
 
-                // sass 编译
-                .pipe(sass().on('error', sass.logError))
+                    // sass 编译
+                    .pipe(sass().on('error', sass.logError))
 
-                // 解除 import 注释
-                .pipe(replace(/(\/\*\*\s{0,})(@.+)(\s{0,}\*\*\/)/g, ($1, $2, $3) => $3.replace(/\.scss/g, '.wxss')))
+                    // 解除 import 注释
+                    .pipe(replace(/(\/\*\*\s{0,})(@.+)(\s{0,}\*\*\/)/g, ($1, $2, $3) => $3.replace(/\.scss/g, '.wxss')))
 
-                // 文件重命名为 wxss
-                .pipe(rename({ extname: '.wxss' }))
+                    // 文件重命名为 wxss
+                    .pipe(rename({ extname: '.wxss' })),
+            );
 
-                // 写入目标目录
+            // 写入目标目录
+            afterCompiled.push(stream => stream
                 .pipe(gulp.dest(to))
                 .on('end', () => resolve({ toRemoveFiles }))
-                .on('error', reject);
+                .on('error', reject));
+
+            // 执行任务
+            afterCompiled.reduce((preResult, curTask) => curTask(preResult));
         }))
 
         // 清理白名单样式
